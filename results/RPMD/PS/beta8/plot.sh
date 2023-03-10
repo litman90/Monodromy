eed446252dcd7c992544870e3abd2f9bf5795b53
Yair Litman <yairlitman@gmail.com>
2023-03-10 08:17:39 +0000

    add some plot.sh files

---
+++ results/RPMD/PS/beta8/plot.sh
@@ -0,0 +1,3 @@
+asym=RPMD_thermal_R2_mildly_anharmonic_a_-0.605_b_0.427_T_0.125_nbeads_32_dt_0.01_asym.txt
+sym=RPMD_thermal_R2_mildly_anharmonic_a_-0.605_b_0.427_T_0.125_nbeads_32_dt_0.01_sym.txt
+  python ../../../../scripts/FFT_2D.py -file1 ${sym} --file2  ${asym} -n1 401 -n2 401 -beta 8.0 -dt 0.1 -lmax 20
